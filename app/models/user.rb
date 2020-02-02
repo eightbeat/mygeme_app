4b1bec4a5270ce50c9846ace2d7e8dc01f1d1daf
eightbeat <sansei.kid011@gmail.com>
2020-02-02 10:17:05 +0900

    Add user edit function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                           format: { with: VALID_EMAIL_REGEX },
                           uniqueness: { case_sensitive: false }
-
+  validates :password, presence: true, length: { minimum: 5 },  allow_nil: true  #allow_nil: true 対象の値がnilの場合にバリデーションをスキップ
   has_many :games
   has_secure_password
 end
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create!(name: "Taro", email: "Taro@example.com", password: "password", password_confirmation: "password")
+  end
+
+  test "reject an invalid edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: " ", email: " " } }
+    assert_template 'users/edit'
+  end
+
+  test "accept valid edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "taro1", email: "taro1@example.com"} }
+    assert_redirected_to @user
+    assert_not flash.empty?
+    @user.reload
+    assert_match "taro1", @user.name
+    assert_match "taro1@example.com", @user.email
+  end
+end
